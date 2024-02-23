ec941abeceae3a6246d6285e1e9805e490f7e1d7
Asher <15080625+AsherFaisal@users.noreply.github.com>
2024-02-22 23:00:11 -0500

    Add files via upload
    Improved functionality. It was using file name with extensions. If extensions were not the same it would not function. Cleaned up code to take inputs

--- filter.py
+++ filter.py
@@ -1,8 +1,8 @@
 import os, shutil
 
-path1 = r'C:\Users\Asher\Documents\Code\Folder filter\test'
-path2 = r'C:\Users\Asher\Documents\Code\Folder filter\test\T2'
-merge = r'C:\Users\Asher\Documents\Code\Folder filter\Merged'
+path1 = input("Enter path - This where want it to copy FROM\t")
+path2 = input("Enter path - This is where it should compare with\t")
+merge = input("Enter destination folder\t")
 
 if not (os.path.exists(merge)):
     os.umask(0)
@@ -12,8 +12,11 @@ dirPath1 = os.listdir(path1)
 dirPath2 = os.listdir(path2)
 count = 0
 
+p2FileList = list(map(lambda x: os.path.splitext(x)[0], dirPath2))
+
 for i in dirPath1:
-    if i in dirPath2:
+    file = os.path.splitext(i)[0]
+    if file in p2FileList:
         filepath = os.path.join(path1, i)
         shutil.copy(f"{path1}\\{i}", merge)
         count += 1
